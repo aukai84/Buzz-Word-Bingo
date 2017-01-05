4ad2dc68f9d5ff8b43c7101e9e1f133f14479def
Aukai Tirrell <mtirrell@hawaii.edu>
2017-01-04 22:08:44 -1000

    added put request to update current objects

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const qs = require('querystring');
 
 let app = express();
 let buzzWords = [];
-let buzzWordNames = [];
 
 app.use((req, res, next) => {
   console.log("first request");
@@ -17,15 +16,21 @@ app.use(bodyParser.urlencoded());
 
 app.post('/buzzword', (req, res, next) => {
   buzzWords.push(req.body);
-  buzzWordNames.push(req.body.buzzWord);
+  console.log(`added ${req.body} to buzzWords array`);
   res.json({"success": true});
   next();
 });
 
-app.put('buzzword', (req, res, next) => {
-  if(buzzWordNames.indexOf(body.req.buzzWord) > -1){
-
+app.put('/buzzword', (req, res, next) => {
+  let newWord = req.body;
+  for(let i = 0; i < buzzWords.length; i++){
+    if(buzzWords[i].buzzWord === newWord.buzzWord){
+      buzzWords[i].buzzWord = newWord.buzzWord;
+      buzzWords[i].points = newWord.points;
+      buzzWords[i].heard = true;
+    }
   }
+  res.json({"success": true, newScore: newWord.points});
 });
 
 app.get('/', (req, res, next) => {
@@ -33,12 +38,13 @@ app.get('/', (req, res, next) => {
   //   if(err){
   //     throw "Error";
   //   }
+    console.log('getting index.html...');
     res.sendFile(__dirname + '/public/index.html');  // });
 });
 
 app.get('/buzzwords', (req, res, next) => {
-  res.json({"buzzWords": buzzWords,
-"buzzWordNames": buzzWordNames});
+  console.log('sending buzzWords content to client');
+  res.json({"buzzWords": buzzWords});
 });
 
 let server = app.listen(3000, () => {
